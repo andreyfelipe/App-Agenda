42c26f0a303e0f264a97c48144b86c81a775707e
Andrey Felipe <andreyfelipe22@gmail.com>
2019-10-21 23:42:30 -0300

    Commit criando projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace App_Agenda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
